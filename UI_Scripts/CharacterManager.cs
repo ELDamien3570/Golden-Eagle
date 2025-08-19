fe387cd6f2a0ad894149061dab521ad0559ff623
ELDamien3570 <damienhelm@gmail.com>
2025-08-19 10:14:51 -0400

    Completely clear repository

--- UI_Scripts/CharacterManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class CharacterManager : MonoBehaviour
-{
-    public ItemSlot[] characterItemSlots;
-    public ItemSlot[] characterWeaponSlots;
-    public ItemSlot[] characterArmorSlots;
-}
